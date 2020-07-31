b5ec5b6d6522e6703c295b2fb569933111a3849f
JavierDominguezGomez <jdomgo3@gmail.com>
2020-07-31 17:25:56 +0200

    SignUp form done.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# not-frontend
-Not Social Network (Front-end)
+# Not Social Network (Front-end)
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import SignUpLogIn from "./page/SignUpLogIn";
+import { ToastContainer } from "react-toastify";
+import { AuthContext } from "./utils/context";
+import { isUserLoggedInApi } from "./api/auth";
+
+export default function App() {
+  const [user, setUser] = useState(null);
+  const [loadUser, setLoadUser] = useState(false);
+  const [refreshCheckLogIn, setRefreshCheckLogIn] = useState(false);
+
+  useEffect(() => {
+    setUser(isUserLoggedInApi());
+    setRefreshCheckLogIn(false);
+    setLoadUser(true);
+  }, [refreshCheckLogIn]);
+
+  if (!loadUser) return null;
+
+  return (
+    <AuthContext.Provider value={user}>
+      {user ? (
+        <h1>You are logged in</h1>
+      ) : (
+        <SignUpLogIn setRefreshCheckLogIn={setRefreshCheckLogIn} />
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnVisibilityChange
+        draggable
+        pauseOnHover
+      />
+    </AuthContext.Provider>
+  );
+}
