3bb9dfd855241aafca6c50c2e20f8c386cd7a134
JavierDominguezGomez <jdomgo3@gmail.com>
2020-08-09 19:59:08 +0200

    Updates

---
+++ precache-manifest.76cf0d64870f2d128aece3c4a3f7c6f1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2e6260d5afc259e5c0cca2932eab8f",
+    "url": "/not-frontend/index.html"
+  },
+  {
+    "revision": "41e554f01fd7b2c59a5e",
+    "url": "/not-frontend/static/css/2.ff572f47.chunk.css"
+  },
+  {
+    "revision": "e7c841c6fe5e52df6d6a",
+    "url": "/not-frontend/static/css/main.fb5e8c59.chunk.css"
+  },
+  {
+    "revision": "41e554f01fd7b2c59a5e",
+    "url": "/not-frontend/static/js/2.6154f194.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/not-frontend/static/js/2.6154f194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c841c6fe5e52df6d6a",
+    "url": "/not-frontend/static/js/main.b64023aa.chunk.js"
+  },
+  {
+    "revision": "5bc549f9053f8e696817",
+    "url": "/not-frontend/static/js/runtime-main.ae202dc4.js"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/not-frontend/static/media/camera.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/not-frontend/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/not-frontend/static/media/date-of-birth.9b45fe49.svg"
+  },
+  {
+    "revision": "d3c57e3b34eea06328b31dbb81bcabc4",
+    "url": "/not-frontend/static/media/error-404.d3c57e3b.png"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/not-frontend/static/media/location.02f2070b.svg"
+  },
+  {
+    "revision": "d3bb4256b745e6f165fc9a8f910b57d2",
+    "url": "/not-frontend/static/media/not-logo-white.d3bb4256.png"
+  },
+  {
+    "revision": "b7fdcd5653c7998c32e63759c80b49e2",
+    "url": "/not-frontend/static/media/not-logo.b7fdcd56.png"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/not-frontend/static/media/web-site.a108520e.svg"
+  }
+]);
